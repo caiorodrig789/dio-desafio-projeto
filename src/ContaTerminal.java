091b06544e95a8587fed8caaa19bc91d47330081
CAIO <CAIO@CAIO>
2024-06-05 19:54:22 -0300

    commit final

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,4 +1,3 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
@@ -29,8 +28,6 @@ public class ContaTerminal {
 
         System.out.println("Por favor, digite o saldo:");
         double saldo = sc.nextDouble();
-		Locale.setDefault(Locale.US);
-
 
         // Exibe a mensagem de confirmação
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta" + numero + " e seu saldo " + saldo + " já está disponível para saque.");
